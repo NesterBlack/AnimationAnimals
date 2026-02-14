9a25fe953a2b616f25a4d120f08f5b3144be2655
Nester <nesterblack13@gmail.com>
2026-02-14 17:37:23 +0200

    change variable name

--- animation.py
+++ animation.py
@@ -153,7 +153,7 @@ class Animal:
             pygame.draw.line(surface, self.color, self.body[-1].pos, (posX, posY), 10)
 
 
-            direction = (self.body[0].left_point_pos - self.body[0].pos)*self.size_factor*1.5
+            direction = (self._body[0].left_point_pos - self._body[0].pos) * self.size_factor * 1.5
             direction = direction.rotate(45)
             posX = direction.x + self._body[0].left_point_pos[0]
             posY = direction.y + self._body[0].left_point_pos[1]
