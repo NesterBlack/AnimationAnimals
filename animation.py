2095232252b5f72faf48bc180a6c8a49d2617f60
Nester <nesterblack13@gmail.com>
2026-02-14 17:39:56 +0200

    delete animal name, because it creates a class for everyone

--- animation.py
+++ animation.py
@@ -37,7 +37,7 @@ class BodyBall:
 
 
 class Animal:
-    def __init__(self, surface: pygame.Surface, animal_name: str="slug", size_factor: float=1.0, color: tuple=(79, 227, 134), debug: bool=False):
+    def __init__(self, surface: pygame.Surface, size_factor: float=1.0, color: tuple=(79, 227, 134), debug: bool=False):
         self.surface = surface
         self.animal_name = animal_name
         self.size_factor = size_factor
