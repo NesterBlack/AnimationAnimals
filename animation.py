22332e6ed8a074d94e6449e8585a449b5ea3bef2
Nester <nesterblack13@gmail.com>
2026-02-21 19:04:33 +0200

    remove pygame parts to make the code universal

--- animation.py
+++ animation.py
@@ -114,7 +114,5 @@ class Slug(Animal):
     def __init__(self, size_factor: float = 1.0, color: tuple = (79, 227, 134), debug: bool = False):
         self.animal_name = "slug"
         self.size_factor = size_factor
-        self.color = color
-        self.debug = debug
         self._body = self._create_animal(self.animal_name, self.size_factor)
 
