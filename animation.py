66be0989b68126d34c493b3a39c63e8753a834a7
Nester <nesterblack13@gmail.com>
2026-02-21 11:09:53 +0200

    delete func

--- animation.py
+++ animation.py
@@ -2,17 +2,6 @@ import pygame
 import math
 from shapely.geometry import Polygon
 
-def subtract_tuples(*args) -> tuple():
-    lens_tuples = {}
-    len_tuple = 0
-    for tup in args:
-        if lens_tuples.get(len(tup)):
-            lens_tuples[len(tup)] += 1
-        else:
-            lens_tuples[len(tup)] = 1
-        len_tuple = len(tup)
-    if len(lens_tuples) > 1:
-        raise Exception("tuples have different lengths")
 
     result = list(args[0])
     for tup in args[1:]:
