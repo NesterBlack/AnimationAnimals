dbfd2f878b5d204546aef16b94e6e5c6ebafb210
Nester <nesterblack13@gmail.com>
2026-02-28 09:46:27 +0200

    test commit 2

--- animation.py
+++ animation.py
@@ -1,15 +1,21 @@
-import math
-from math import radians
+from math import radians, cos, sin, fabs, degrees, atan2
 import numpy as np
 from numpy import array
 from shapely.geometry import Polygon
+
+print("""
+=====HELP=====
+func get_points_for_draw
+    Slug -- (points body for polygon, pos eyes)
+    Bird -- (points body for polygon, pos eyes, points wings for polygon)
+""")
 
 def rotation_vector2(vector: array, angle: float) -> array:
     rad_angle = radians(angle)
     new_vector = array([0,0], dtype=float)
 
-    new_vector[0] = vector[0] * math.cos(rad_angle) - vector[1] * math.sin(rad_angle)
-    new_vector[1] = vector[0] * math.sin(rad_angle) + vector[1] * math.cos(rad_angle)
+    new_vector[0] = vector[0] * cos(rad_angle) - vector[1] * sin(rad_angle)
+    new_vector[1] = vector[0] * sin(rad_angle) + vector[1] * cos(rad_angle)
 
     return new_vector
 
@@ -18,8 +24,8 @@ class BodyBall:
         self.pos = array([0,0])
         self.angle = 0
 
-        self.left_point_pos = None
-        self.right_point_pos = None
+        self.left_point_pos = array([0,0])
+        self.right_point_pos = array([0,0])
 
         self.forward_point_pos = None
         self.left_forward_point_pos = None
@@ -31,31 +37,51 @@ class BodyBall:
 
         self.radius = radius
 
+class Wings:
+    def __init__(self, amplitude: float = 1.5, speed: float = 10, time: float = 0):
+        self.amplitude = amplitude
+        self.speed = speed
+        self.time = time
 
+        self.points = array([])
 
+    def update_wings(self, position: array, angle: float):
+        points = []
+        x = -self.amplitude
+        while x < self.amplitude:
+            y = fabs(x) * sin(fabs(x) + self.speed * self.time)
+            dir = array([(x * 100), (y * 100)])
+            dir = rotation_vector2(dir, angle)
+            points.append(position+dir)
+            x += 10 ** -2
+        self.time += 10**-3
+
+        self.points = array(points)
 
 class Animal:
     def __init__(self, size_factor: float=1.0):
         self.animal_name = "animal name"
         self.size_factor = size_factor
-        self._body = self._create_animal(self.animal_name, self.size_factor)
+        self._body = self._create_animal()
 
     # TODO: extract form class
-    def _create_animal(self, animal="slug", size_factor=1) -> [BodyBall]:
+    def _create_animal(self) -> [BodyBall]:
         balls = []
-        if animal.lower() == "slug":
-            size = [x * size_factor for x in [34, 42, 43, 42, 41, 38, 32, 30, 25, 19, 17, 16, 9, 7]]
+        if self.animal_name.lower() == "slug":
+            size = [x * self.size_factor for x in [34, 42, 43, 42, 41, 38, 32, 30, 25, 19, 17, 16, 9, 7]]
+        if self.animal_name.lower() == "bird":
+            size = [x * self.size_factor for x in [20,25,30,35,36,35,30,15]]
         for i in range(len(size)):
             balls.append(BodyBall(size[i]))
             balls[-1].pos[0] = i*max(size)
-            balls[-1].pos[1] = 0
+            balls[-1].pos[1] = 1
 
         return balls
 
     
     def move_animal(self, go_to: tuple, speed):
         direction = self._body[0].pos - go_to
-        self._body[0].angle = math.degrees(math.atan2(direction[1], direction[0]))
+        self._body[0].angle = degrees(atan2(direction[1], direction[0]))
         distance = np.linalg.norm(direction)
         if distance != 0:
             direction = direction / distance
@@ -78,7 +104,7 @@ class Animal:
             distance = np.linalg.norm(direction)
             if distance != 0:
                 direction = direction / distance
-                ball.angle = math.degrees(math.atan2(direction[1], direction[0]))
+                ball.angle = degrees(atan2(direction[1], direction[0]))
                 ball.pos = prev - direction * ball.radius
                 ball.left_point_pos = ball.pos + rotation_vector2(direction, 90) * ball.radius
                 ball.right_point_pos = ball.pos + rotation_vector2(direction, -90) * ball.radius
@@ -107,12 +133,124 @@ class Animal:
             points = list(fixed.exterior.coords)
         except AttributeError or TypeError:
             pass
-        return (points, [self._body[0].left_forward_point_pos, self._body[0].right_forward_point_pos])
+        return (points, [self._body[0].left_forward_point_pos, self._body[0].right_forward_point_pos], )
+
+    def debug(self):
+        result = list()
+
+        skeletons = list()
+        points = list()
+        body = list()
+
+        for index, ball in enumerate(self._body):
+            if index:
+                direction = rotation_vector2(array([0,1]) * ball.radius, ball.angle-90)
+            else:
+                direction = rotation_vector2(array([0, 1]) * ball.radius, ball.angle+90)
+
+            angle_pos = ball.pos + direction
+
+            skeletons.append((ball.pos, angle_pos))
+
+            points.append(ball.left_point_pos)
+            points.append(ball.right_point_pos)
+            print(ball.forward_point_pos, ball.left_backward_point_pos, ball.right_backward_point_pos, "123456789")
+            if ball.forward_point_pos is not None:
+                points.append(ball.forward_point_pos)
+                points.append(ball.left_forward_point_pos)
+                points.append(ball.right_forward_point_pos)
+            elif ball.backward_point_pos is not None:
+                points.append(ball.backward_point_pos)
+                points.append(ball.left_backward_point_pos)
+                points.append(ball.right_backward_point_pos)
+
+            body.append((ball.pos, ball.radius))
+
+        result.append(skeletons)
+        result.append(points)
+        result.append(body)
+
+        return result
 
 
 class Slug(Animal):
     def __init__(self, size_factor: float = 1.0):
         self.animal_name = "slug"
         self.size_factor = size_factor
-        self._body = self._create_animal(self.animal_name, self.size_factor)
+        self._body = self._create_animal()
 
+    def get_points_for_draw(self):
+        points = [self._body[0].forward_point_pos, self._body[0].left_forward_point_pos]
+        for ball in self._body:
+            points.append(ball.left_point_pos)
+        points.append(self._body[-1].right_backward_point_pos)
+        points.append(self._body[-1].backward_point_pos)
+        points.append(self._body[-1].left_backward_point_pos)
+        for ball in self._body[::-1]:
+            points.append(ball.right_point_pos)
+        points.append(self._body[0].right_forward_point_pos)
+        poly = Polygon(points)
+        fixed = poly.buffer(0)
+        try:
+            points = list(fixed.exterior.coords)
+        except AttributeError or TypeError:
+            pass
+        return (points, [self._body[0].left_forward_point_pos, self._body[0].right_forward_point_pos])
+
+    def debug(self):
+        result = super().debug()
+        return result
+
+class Bird(Animal):
+    def __init__(self, size_factor: float = 1.0, amplitude: float = 1.5, speed: float = 10):
+        self.animal_name = "bird"
+        self.size_factor = size_factor
+        self._body = self._create_animal()
+
+        self._wings = Wings(amplitude, speed)
+        self.wings_poly_points = array([])
+        self.pin_body_index_wings = 3
+
+    def get_points_for_draw(self):
+        points = [self._body[0].forward_point_pos, self._body[0].left_forward_point_pos]
+        for ball in self._body:
+            points.append(ball.left_point_pos)
+        points.append(self._body[-1].right_backward_point_pos)
+        points.append(self._body[-1].backward_point_pos)
+        points.append(self._body[-1].left_backward_point_pos)
+        for ball in self._body[::-1]:
+            points.append(ball.right_point_pos)
+        points.append(self._body[0].right_forward_point_pos)
+        poly = Polygon(points)
+        fixed = poly.buffer(0)
+        try:
+            points = list(fixed.exterior.coords)
+        except AttributeError or TypeError:
+            pass
+
+        return (points, [self._body[0].left_forward_point_pos, self._body[0].right_forward_point_pos], self.wings_poly_points)
+
+    def move_animal(self, go_to: tuple, speed):
+        super().move_animal(go_to, speed)
+
+        self._wings.update_wings(self._body[self.pin_body_index_wings].pos, self._body[self.pin_body_index_wings].angle+90)
+        width = 50
+        len_line = []
+        for index, n in enumerate(self._wings.points):
+            if index < len(self._wings.points) // 2:
+                len_line.append(width / (len(self._wings.points) / 2) * (index + 1))
+        for i in len_line.copy()[::-1]:
+            len_line.append(i)
+
+        points_for_draw = []
+        for index, l in enumerate(len_line):
+            points_for_draw.append((self._wings.points[index][0], self._wings.points[index][1] - (l / 2)))
+        for index, l in enumerate(len_line[::-1]):
+            real_index = len(len_line) - 1 - index
+            points_for_draw.append((self._wings.points[real_index][0], self._wings.points[real_index][1] + (l / 2)))
+
+        self.wings_poly_points = points_for_draw
+
+    def debug(self):
+        result = super().debug()
+        return result
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env -S uv run --script
-# /// script
-# dependencies = ["pygame", "shapely"]
-# ///
-
 import pygame
-from animation import Animal
+from animation import *
 
 
 pygame.init()
@@ -14,7 +9,10 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 font = pygame.font.SysFont("Arial", 40)
 clock = pygame.time.Clock()
 
-animal = Animal(screen, "robocode", 0.5, (82, 179, 227), debug=False)
+pos = pygame.Vector2(0,0)
+direction = pygame.Vector2(0,0)
+
+animal = Bird()
 running = True
 if __name__ == "__main__":
     while running:
@@ -23,7 +21,25 @@ if __name__ == "__main__":
                 running = False
         screen.fill((0, 0, 0))
         animal.move_animal(pygame.mouse.get_pos(), 0.1)
-        animal.draw_animal(screen)
+
+        attributes = animal.get_points_for_draw()
+        pygame.draw.polygon(screen, (82, 179, 227), attributes[0], 0)
+
+        print(attributes[2])
+        pygame.draw.polygon(screen, (82, 100, 227), attributes[2], )
+
+        for point in attributes[1]:
+            pygame.draw.circle(screen, (255,255,255), point, 10)
+            pygame.draw.circle(screen, (0, 0, 0), point, 4)
+
+        debug = animal.debug()
+        # for line in debug[0]:
+        #     pygame.draw.line(screen, (255, 255, 255), line[0], line[1], 1)
+        # for points in debug[1]:
+        #     print(points)
+        #     pygame.draw.circle(screen, (255, 0, 0), points, 5)
+        # for par in debug[2]:
+        #     pygame.draw.circle(screen, (0, 255, 0), par[0], par[1], 3)
 
         pygame.display.flip()
-        clock.tick(60)
+        # clock.tick(60)
